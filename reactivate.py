0f271f0fe751646ec9425d34a7cdff1d21b31c69
YC酱luyancib <luyancib@qq.com>
2025-02-20 23:19:47 +0800

    添加获取用户粉丝牌对应直播间号的功能，并优化cookie刷新逻辑 Note:目前从用户空间获取的方法可能无法使用，会被自动跳过

--- reactivate.py
+++ reactivate.py
@@ -10,8 +10,9 @@ import time
 import random
 from loguru import logger
 from dotenv import load_dotenv
-from bilibili_api import Credential,sync,Danmaku
+from bilibili_api import Credential,sync,Danmaku,user,parse_link
 from bilibili_api.live import LiveRoom
+from bilibili_api.exceptions import ResponseCodeException,ApiException
 from danmu_dict import danmu_list
 
 c=Credential()
@@ -20,7 +21,6 @@ print(sys.argv)
 if len(sys.argv) >= 1:
     if sys.argv[1] == "dev":
         load_dotenv(dotenv_path="./.env")
-
 
 
 def login(): # get cookie from env
@@ -35,6 +35,11 @@ def login(): # get cookie from env
             dedeuserid=cookie.get('DedeUserID'),
             ac_time_value=cookie.get('ac_time_value')
             )
+        if sync(c.check_refresh()):
+            c.refresh()
+            logger.info('Refresh cookie successfully!')
+        else:
+            logger.info('Cookie is still valid!')
     except:
         raise
     return c
@@ -42,8 +47,7 @@ def login(): # get cookie from env
 def reactivate(): # main function
     roomids=os.environ['roomids']
     if roomids == "all":
-        roomids=roomids.split(",") # WIP: get all roomids from medal_list
-        pass
+        roomids=get_roomids_for_medal()
     else:    
         roomids=roomids.split(",")
     try:
@@ -58,6 +62,45 @@ def reactivate(): # main function
                 time.sleep(sleep_time)
     except:
         raise
+
+def get_roomids_for_medal(): # get roomids from medal list(but user class doesn't work)
+    user_self=sync((user.get_self_info(credential=c)))
+    time.sleep(1+random.random())
+    medal_list=sync(user.User(user_self['mid'],credential=c).get_user_medal())
+    logger.info('Get medal list successfully!')
+    medal_list=medal_list['list']
+    roomids=[]
+    for i in medal_list:
+        logger.info(f"获取当前牌子（{i['medal_info']['medal_name']}）的对应直播间号，目前已成功获取的直播间号列表为：{roomids}")
+        try:
+            if i['link'].startswith('https://space.bilibili.com/'): # idk why it doesn't work
+                logger.info(f"获取到个人空间链接，尝试获取直播间号...")
+                obj=sync(parse_link(url=i['link'],credential=c))[0]
+                time.sleep(3+random.random())
+                print(sync(obj.get_access_id()))
+                info=sync(obj.get_user_info())
+                print(info['live_room'])
+                roomids.append(info['live_room']['roomid'])
+            elif i['link'].startswith('https://live.bilibili.com'): # it works when is streaming
+                logger.info(f"获取到直播间链接，尝试获取直播间号...")
+                roomid=str()
+                roomid=i['link'].replace('https://live.bilibili.com/','')
+                index=roomid.find('?')
+                roomid=roomid[:index]
+                roomids.append(roomid)
+            else:
+                logger.warning('Unknown link: '+i['link'])
+        except ResponseCodeException as e:
+            logger.warning(f"获取失败，原因：{e}，跳过")
+            continue
+        except ApiException as e:
+            logger.warning(f"获取失败，原因：{e}，跳过")
+            continue
+        logger.info(f"获取成功")
+        time.sleep(3+random.random())
+    return roomids
+    
+    
 
 if __name__ == "__main__":
     timer=time.perf_counter()
